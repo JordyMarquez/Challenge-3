6243f2cfc0a0cd2ba4a5477e2d8f9c2cd3e4443f
Jordy Marquez <jordymarquez@MacBook-Pro.local>
2022-12-22 21:17:07 -0800

    restructured files and added javascript code

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,81 @@
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// Write password to the #password input 
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Naming the different criteria
+var lowercaseLetter = 'abcdefghijklmnopqrstuvwxyz';
+var lowercaseArray = lowercaseLetter.split('');
+var uppercaseLetter = lowercaseLetter.toUpperCase();
+var uppercaseArray = uppercaseLetter.split('');
+var symbols = '!@#$%^&*(){}[]=<>/,.'.split('');
+var numericValues = '0123456789'.split('');
+
+var passwordCharCount = 0  
+var shouldUseLowerCase = true
+var shouldUseUpperCase =  true 
+var shouldUseSymbols = true 
+var userChoiceArray = []
+
+
+// Makes a function to generate a password
+function generatePassword() {
+var password = ""
+
+// Sets parameter to be between 8 & 128 characters
+   passwordCharCount = prompt("How many characters would you like to use? Must be between 8 and 128 characters")
+   if(passwordCharCount < 8 || passwordCharCount > 128 || !Number(passwordCharCount)){
+    alert('please follow instructions');
+    return "please click button again"
+   }
+
+// Asks user to select password criteria and creates an array
+   shouldUseLowerCase = confirm("Do you want to use lower case letters?")
+   if (shouldUseLowerCase) {
+    userChoiceArray = userChoiceArray.concat(lowercaseArray)
+    console.log(userChoiceArray)
+   }
+   
+   shouldUseUpperCase = confirm("Do you want to use upper case letters?")
+   if (shouldUseUpperCase) {
+    userChoiceArray = userChoiceArray.concat(uppercaseArray)
+   }
+   console.log(userChoiceArray)
+
+   shouldUseSymbols = confirm("Do you want to use symbols?")
+   if (shouldUseSymbols) {
+    userChoiceArray = userChoiceArray.concat(symbols)
+   }
+   console.log(userChoiceArray)
+
+   shouldUseNumbers = confirm("Would you like to use numbers?")
+   if (shouldUseNumbers) {
+    userChoiceArray = userChoiceArray.concat(numericValues)
+   }
+   console.log(userChoiceArray)
+
+   if(userChoiceArray.length === 0){
+    alert('No criteria was selected')
+    return "please click button again"
+   }
+
+// Randomizes the array
+userChoiceArray.sort(function(a,b){
+ var randNum = Math.floor(Math.random() * 2)
+ return randNum === 0 ? -1 : 1
+})
+   console.log("shuffled", userChoiceArray)
+// Make the password as long as requested by user
+   for (var i = 0; i < passwordCharCount; i++) {
+    password += userChoiceArray[i];
+   }
+  return password
+  }
